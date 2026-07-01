b61ceed6975511e370fa08cbc39ef0345f2ed4e2
Felipe-MDB <femarques.brito@outlook.com>
2026-07-01 13:00:34 -0300

    axios

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import Navbar from './components/navbar/Navbar'
 import Cadastro from './pages/cadastro/cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/login'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
 	return (
 		<>
-			<BrowserRouter>
-				<Navbar />
-				<div className="min-h-[80vh]">
+			<AuthProvider>
+				<BrowserRouter>
+					<Navbar />
+					<div className="min-h-[80vh]">
 					<Routes>
 						<Route path="/" element={<Login />} />
 						<Route path="/home" element={<Home />} />
 						<Route path="/cadastro"	element={<Cadastro />}/>
 					</Routes>
-				</div>
-				<Footer />
-			</BrowserRouter>
+					</div>
+					<Footer />
+				</BrowserRouter>
+			</AuthProvider>	
 		</>
 	)
 }
